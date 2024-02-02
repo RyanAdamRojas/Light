6e4d4b0edde180d298e75cff0ed97d838e585d03
Ryan Rojas <ryanadamrojas@protonmail.com>
2024-02-02 08:33:19 -0700

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Ryan Adam Rojas on 5/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    fileprivate func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+}
+
